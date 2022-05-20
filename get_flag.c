a81cc83e84dcf607a954180c3e1f7f6f4e704b0b
MohammedZalvet11 <mohammedzalvet@gmail.com>
2022-05-19 17:28:21 -0700

    get_flag.c

---
+++ get_flag.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ *
+ */
+int get_flag(char s, flags_t *f)
+{
+	int i = 0;
+
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
+
+	return (i);
+}
